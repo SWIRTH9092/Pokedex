708c05e04aa2a904de899c3b1d6e8b3a20946d6e
Your Name <you@example.com>
2022-11-16 10:06:20 -0500

    added ejs and view files.  Base index working

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,9 @@ const express = require('express')
 //get MethodOverride Package
 const methodOverride = require("method-override") // import method override
 
+//instead of app=express
+const router = express.Router();
+
 router.use("/static", express.static("public")) // it's going serve files from a folder called "public" under /static example public/styles.css => /static/styles.css
 
 //Update the location of the data
@@ -13,14 +16,20 @@ const pokemon = require('../models/pokemon')
 // for ejs-helpers 
 const helpers = require("./ejs-helpers.js")
 
-//instead of app=express
-const router = express.Router();
 
-//note router.get
-router.get('/', (req, res) => {
-    console.log("In get router")
-    res.send(`<h1>Get worked from Index File </h1>`)
-})
 
+// HOME ROUTE - Just redirects you to budgets index for now
+router.get("/", (req, res) => res.redirect("/Pokedex"))
+
+// INDEX ROUTE - GET to / - Returns all Budget Items
+router.get('/Pokedex', (req,res) => {
+    // res.render(template, data)
+    res.render(
+        'index.ejs',
+        {
+            allpokemon:pokemon
+        }
+    );
+});
 //make this router exportable
 module.exports = router;
