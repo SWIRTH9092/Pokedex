db23f34c5458d8767f99f820444f60e3c7a58a32
Your Name <you@example.com>
2022-11-15 21:15:01 -0500

    base install and prep

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const morgan = require("morgan")  // import Morgan (logging middleware)
+const PORT = process.env.POR || 3000; // (in case of env file missing)
+
+
+app.listen(3000, ()=> {
+    console.log("listening on port:", PORT)
+})
