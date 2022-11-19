3a5e5c36f971ffb1aaa5d610ef6d1ce8bdf33c24
Your Name <you@example.com>
2022-11-19 08:25:37 -0500

    updated for length encoding; Type displaying and updating

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,27 @@
 //get express package
-// const { response, application } = require('express');
+const { response, application } = require('express');
 const express = require('express')
 
 //get MethodOverride Package
 const methodOverride = require("method-override") // import method override
-
-//instead of app=express
-const router = express.Router();
-
-//Method over-ride
-router.use(methodOverride("_method")) // swap the method if the url has a ?_method=XXX query
-
-router.use("/static", express.static("public")) // it's going serve files from a folder called "public" under /static example public/styles.css => /static/styles.css
 
 //Update the location of the data
 const pokemon = require('../models/pokemon')
 
 // for ejs-helpers 
 const helpers = require("./ejs-helpers.js")
+
+//instead of app=express
+const router = express.Router();
+router.use("/Pokedex", express.urlencoded({extended: true})) 
+
+//Method over-ride
+router.use(methodOverride("_method")) // swap the method if the url has a ?_method=XXX query
+
+router.use("/static", express.static("public")) // it's going serve files from a folder called "public" under /static example public/styles.css => /static/styles.css
+
+
+
 
 // set the default template extension
 
@@ -44,5 +48,60 @@ router.delete("/Pokedex/:index", (req, res) => {
     res.redirect("/Pokedex")  
   })
 
+// EDIT route = GET to /Pokedex/:index/edit - render a form to edit the Pokemon Info
+router.get("/Pokedex/:index/edit", (req, res) => {
+  // render edit.ejs with the existing Pokemon data
+  console.log("pokemonInfo - edit:", pokemon[req.params.index] )
+  res.render("edit.ejs", {
+    pokemonInfo: pokemon[req.params.index],
+    index: req.params.index,  
+    helpers 
+  })
+})
+
+router.put("/Pokedex/:index", (req, res) => {
+  console.log(req.header)
+  console.log("req.body", req.body)
+  // console.log("req.body.type",req.body.type)
+  // convert amount from string to number
+  // let workAmount = 0;
+  // if (!req.body.amount) {
+  //    workAmount = 0
+  // }  else{
+  //    workAmount = parseInt(req.body.amount)
+  // }
+ 
+  // req.body.amount = workAmount
+ 
+  // convert tags to an array
+  // let workTags = [];
+  // workTags = req.body.tags.split(",")
+ 
+  //removed leading and trailing spaces from array element
+  // workTags = workTags.map (function (el){
+  //     return el.trim();
+  // });
+  // req.body.tags = workTags
+     
+  // // updating Budget
+  //    Budget[req.params.index] = req.body
+ 
+  // redirect user back to index
+    //  res.redirect("/Pokedex")
+})
+  
+   
+ 
+
+
+  //SHOW ROUTE - GET to /Pokedex - Returns a info for a pokemon
+router.get("/Pokedex/:index", (req, res) => {
+    // res.render(template, data)
+    res.render("show.ejs", {
+      pokemonInfo: pokemon[req.params.index],
+      index: req.params.index,  
+      helpers  
+    });
+  });
 //make this router exportable
 module.exports = router;
